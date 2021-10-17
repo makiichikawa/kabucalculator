30d81996cf22097549914b750a68884b6c39fd5d
makiichikawa <punkfolkfolk@gmail.com>
2021-10-17 17:55:47 +0900

    例外処理のテストを追加

--- python/test/test_stock.py
+++ python/test/test_stock.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import MagicMock
+from unittest.mock import patch
 import sys
 import os
 import datetime
@@ -10,13 +12,18 @@ from lib.stock import Stock  # noqa: E402
 
 class TestStock(unittest.TestCase):
     def setUp(self):
-        symbol = '1332'
-        from_date = datetime.datetime(2021, 10, 14)
-        to_date = datetime.datetime(2021, 10, 15)
-        country = 'japan'
-        interval = 'Daily'
-        self.stock = Stock(from_date, to_date, country, interval)
-        self.stock.set_prices(symbol)
+        self.symbol = '1332'
+        self.from_date = datetime.datetime(2021, 10, 14)
+        self.to_date = datetime.datetime(2021, 10, 15)
+        self.country = 'japan'
+        self.interval = 'Daily'
+        self.stock = Stock(
+            self.from_date,
+            self.to_date,
+            self.country,
+            self.interval
+            )
+        self.stock.set_prices(self.symbol)
 
     def test_set_stocks(self):
         expect = {
@@ -26,6 +33,20 @@ class TestStock(unittest.TestCase):
             'close': [679, 671]
             }
         self.assertEqual(expect, self.stock.prices)
+
+    @patch(
+        "investpy.get_stock_historical_data",
+        MagicMock(side_effect=IndexError)
+        )
+    def test_set_stocks_error(self):
+        stock = Stock(
+            self.from_date,
+            self.to_date,
+            self.country,
+            self.interval
+            )
+        with self.assertRaises(IndexError):
+            stock.set_prices(self.symbol)
 
 
 if __name__ == "__main__":
