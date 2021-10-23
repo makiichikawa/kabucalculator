d4b62fdd49869b5dae0102009cf5a81e44d120f3
makiichikawa <punkfolkfolk@gmail.com>
2021-10-23 18:23:56 +0900

    MySQLの小数の最大桁数が15を考慮した端数処理を実施

--- python/lib/actualnormparameter.py
+++ python/lib/actualnormparameter.py
@@ -1,4 +1,4 @@
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP
 
 
 class ActualNormParameter:
@@ -9,9 +9,16 @@ class ActualNormParameter:
         self.term = Decimal(str(term))
 
     def calculate_myuhat(self):
-        myuhat = float((self.myu - self.variance / Decimal('2')) * self.term)
-        return myuhat
+        myuhat = ((
+            self.myu - self.variance / Decimal('2')
+            ) * self.term
+        ).quantize(
+            Decimal('0.000000000000001'), rounding=ROUND_HALF_UP
+        )
+        return float(myuhat)
 
     def calculate_sigmahat(self):
-        sigmahat = float((self.variance * self.term).sqrt())
-        return sigmahat
+        sigmahat = ((self.variance * self.term).sqrt()).quantize(
+            Decimal('0.000000000000001'), rounding=ROUND_HALF_UP
+        )
+        return float(sigmahat)
