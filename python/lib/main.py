ff6f08ac737bd05c96d335bf9a4fc635995a58ec
makiichikawa <punkfolkfolk@gmail.com>
2021-11-30 22:15:26 +0900

    Indicatorsクラスを用いてメイン関数をリファクタリング

--- python/lib/main.py
+++ python/lib/main.py
@@ -1,6 +1,5 @@
 import sys
 import datetime
-from decimal import Decimal
 import os
 import investpy
 import mysql.connector as mydb
@@ -9,7 +8,7 @@ sys.path.append('..')
 
 from lib.businessday import BusinessDay  # noqa: E402
 from lib.stock import Stock  # noqa: E402
-from lib.stockmodelprobability import StockModelProbability  # noqa: E402
+from lib.indicators import Indicators  # noqa: E402
 from lib.dbregister import DBRegister  # noqa: E402
 
 TERMS = {
@@ -19,55 +18,17 @@ TERMS = {
 }
 INTERVAL = 'Daily'
 COUNTRY = 'japan'
+
 businessday = BusinessDay()
 to_date = datetime.datetime.now()
 businessday.to_date = to_date
 from_date = businessday.from_date(TERMS['long'])
 
-stock = Stock(from_date, to_date, COUNTRY, INTERVAL)
-probability = StockModelProbability()
-symbols = investpy.stocks.get_stocks_list(COUNTRY)
-indicators = []
-for symbol in symbols:
-    try:
-        stock.set_prices(symbol)
-        prices = stock.prices['close']
-        if len(prices) < TERMS['long']:
-            raise IndexError('prices length is less than long terms')
-    except IndexError as error:
-        print(error.args)
-        print(symbol)
-        continue
-    indicator = []
-    indicator.append(symbol)
-    current_price = prices[-1]
-    indicator.append(current_price)
-    log_returns = stock.calculate_log_return(prices)
-
-    try:
-        for term in TERMS.values():
-            probability.myu = stock.calculate_myu(log_returns[-term:-1])
-            probability.variance = stock.calculate_variance(
-                log_returns[-term:-1]
-            )
-            probability.term = term
-            probability.initial_price = prices[-term]
-            try:
-                upper_probability = probability.calculate_upper_probability(
-                    current_price
-                )
-            except ZeroDivisionError:
-                raise
-            rate = float(Decimal(str(upper_probability)) - Decimal('0.5'))
-            myuhat = probability.myuhat
-            sigmahat = probability.sigmahat
-            indicator.extend([myuhat, sigmahat, rate])
-    except ZeroDivisionError as error:
-        print(error.args)
-        print(symbol)
-        continue
-    else:
-        indicators.append(indicator)
+indicators = Indicators()
+indicators.stock = Stock(from_date, to_date, COUNTRY, INTERVAL)
+indicators.terms = TERMS
+indicators.symbols = investpy.stocks.get_stocks_list(COUNTRY)
+indicators_of_all_symbols = indicators.calculate_indicators_of_all_symbols()
 
 dbregister = DBRegister()
 dbregister.host = os.getenv('DB_HOST')
@@ -80,7 +41,7 @@ dbregister.table = os.getenv('DB_TABLE')
 try:
     dbregister.db_connect()
     dbregister.db_init()
-    dbregister.db_insert(indicators)
+    dbregister.db_insert(indicators_of_all_symbols)
 except mydb.Error as error:
     print(error.arg)
 finally:
