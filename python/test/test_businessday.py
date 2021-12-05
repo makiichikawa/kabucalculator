9f038d437503ec38c6d29fc9f42ba42e79ee26fa
makiichikawa <punkfolkfolk@gmail.com>
2021-12-05 18:52:28 +0900

    取引日を算出する際、開始日が休日の場合に誤りがあったため、修正

--- python/test/test_businessday.py
+++ python/test/test_businessday.py
@@ -10,10 +10,24 @@ from lib.businessday import BusinessDay  # noqa: E402
 class TestBusinessDay(unittest.TestCase):
     def setUp(self):
         self.businessday = BusinessDay()
-        self.businessday.to_date = datetime.datetime(2021, 11, 16)
 
-    def test_startdate(self):
+    def test_from_date_case_to_date_is_weekday(self):
+        self.businessday.to_date = datetime.datetime(2021, 11, 16)
         self.assertEqual(
             datetime.datetime(2021, 11, 2),
             self.businessday.from_date(10)
+        )
+
+    def test_from_date_case_to_date_is_saturday(self):
+        self.businessday.to_date = datetime.datetime(2021, 12, 4)
+        self.assertEqual(
+            datetime.datetime(2021, 12, 1),
+            self.businessday.from_date(3)
+        )
+
+    def test_from_date_case_to_date_is_sunday(self):
+        self.businessday.to_date = datetime.datetime(2021, 12, 5)
+        self.assertEqual(
+            datetime.datetime(2021, 12, 1),
+            self.businessday.from_date(3)
         )
