61e7581942e2751318959442bd30433d776ca58e
makiichikawa <punkfolkfolk@gmail.com>
2021-12-24 22:51:01 +0900

    indicatorsのコントローラテストを追加

--- appserver/test/controllers/indicators_controller_test.rb
+++ appserver/test/controllers/indicators_controller_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class IndicatorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get indicators_path
+    assert_response :success
+    assert_select 'tbody' do
+      assert_select 'tr', 2
+      assert_select 'tr>td', text: '1234'
+    end
+  end
 end
