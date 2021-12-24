1b3585443b773feacdb326eee1be1d2dba94f2ff
makiichikawa <punkfolkfolk@gmail.com>
2021-12-24 22:49:58 +0900

    テスト用にschema.rbを生成

---
+++ appserver/db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "indicators", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+    t.string "symbol", limit: 4, null: false
+    t.integer "price", null: false
+    t.float "myuhats", limit: 53, null: false
+    t.float "sigmahats", limit: 53, null: false
+    t.float "probs", limit: 53, null: false
+    t.float "myuhatm", limit: 53, null: false
+    t.float "sigmahatm", limit: 53, null: false
+    t.float "probm", limit: 53, null: false
+    t.float "myuhatl", limit: 53, null: false
+    t.float "sigmahatl", limit: 53, null: false
+    t.float "probl", limit: 53, null: false
+    t.timestamp "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.timestamp "update_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+  end
+
+end
