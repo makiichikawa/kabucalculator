17f0a6c724e85316ba433221af94119b163569bc
makiichikawa <punkfolkfolk@gmail.com>
2022-01-10 23:17:16 +0900

    indicatorsの表に関して列を追加

--- appserver/app/javascript/indicators-table.vue
+++ appserver/app/javascript/indicators-table.vue
@@ -4,6 +4,16 @@
       tr
         th 銘柄CD
         th 現在価格
+        th 短期上昇率・下降率
+        th 短期リターン
+        th 短期リスク
+        th 中期上昇率・下降率
+        th 中期リターン
+        th 中期リスク
+        th 長期上昇率・下降率
+        th 長期リターン
+        th 長期リスク
+
     tbody
       tr(
         v-for="indicators in allIndicators"
@@ -11,6 +21,15 @@
       )
         td {{indicators.symbol}}
         td {{indicators.price}}
+        td {{indicators.probability_short | addPercent}}
+        td {{indicators.myuhat_short}}
+        td {{indicators.sigmahat_short}}
+        td {{indicators.probability_medium | addPercent}}
+        td {{indicators.myuhat_medium}}
+        td {{indicators.sigmahat_medium}}
+        td {{indicators.probability_long | addPercent}}
+        td {{indicators.myuhat_long}}
+        td {{indicators.sigmahat_long}}
 </template>
 
 <script>
@@ -22,6 +41,12 @@ export default {
   },
   created() {
     this.getIndicators()
+  },
+  filters: {
+    addPercent: function(value){
+      if (!value) return ''
+      return String(value) + '%'
+    }
   },
   methods: {
     token() {
--- appserver/app/views/api/indicators/index.json.jbuilder
+++ appserver/app/views/api/indicators/index.json.jbuilder
@@ -1,4 +1,13 @@
-json.array! @indicators do |indicator|
-	json.symbol indicator.symbol
-	json.price indicator.price
+json.array! @all_indicators do |indicators|
+	json.symbol indicators['symbol']
+	json.price indicators['price']
+	json.probability_short indicators['probability_short']
+	json.myuhat_short indicators['myuhat_short']
+	json.sigmahat_short indicators['sigmahat_short']
+	json.probability_medium indicators['probability_medium']
+	json.myuhat_medium indicators['myuhat_medium']
+	json.sigmahat_medium indicators['sigmahat_medium']
+	json.probability_long indicators['probability_long']
+	json.myuhat_long indicators['myuhat_long']
+	json.sigmahat_long indicators['sigmahat_long']
 end
