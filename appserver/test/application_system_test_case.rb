371608ab0c57a79272565e1ac44b079a42fa997e
makiichikawa <punkfolkfolk@gmail.com>
2022-01-15 21:11:05 +0900

    システムテスト用のSelenium環境を追加

--- appserver/test/application_system_test_case.rb
+++ appserver/test/application_system_test_case.rb
@@ -3,5 +3,17 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  Capybara.default_driver = :selenium_chrome
+  Capybara.register_driver :selenium_chrome do |app|
+    url = ENV['SELENIUM_DRIVER_URL']
+    options = ::Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1400,1400')
+    Capybara::Selenium::Driver.new(app, browser: :remote, url: url, capabilities: [options])
+  end
+  driven_by :selenium_chrome
+  Capybara.server_host = 'appserver'
+  Capybara.app_host = 'http://appserver'
 end
