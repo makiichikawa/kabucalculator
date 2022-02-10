ac2e41cb3aeef74145c697229feaf517d8b8b33d
MakiIchikawa <punkfolkfolk@gmail.com>
2022-02-10 18:26:12 +0900

    クエリ処理に使用するscopeを追加

--- appserver/app/models/indicator.rb
+++ appserver/app/models/indicator.rb
@@ -2,6 +2,55 @@
 
 class Indicator < ApplicationRecord
   self.table_name = 'indicators'
+
+  scope :filter_by_symbols, lambda { |symbol_array|
+    if symbol_array
+      Indicator.where(symbol: symbol_array)
+    else
+      Indicator.all
+    end
+  }
+
+  scope :filter_by_price, lambda { |lowervalue, uppervalue|
+    Indicator.where(price: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_probability_short, lambda { |lowervalue, uppervalue|
+    Indicator.where(probability_short: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_myuhat_short, lambda { |lowervalue, uppervalue|
+    Indicator.where(myuhat_short: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_sigmahat_short, lambda { |lowervalue, uppervalue|
+    Indicator.where(sigmahat_short: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_probability_medium, lambda { |lowervalue, uppervalue|
+    Indicator.where(probability_medium: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_myuhat_medium, lambda { |lowervalue, uppervalue|
+    Indicator.where(myuhat_medium: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_sigmahat_medium, lambda { |lowervalue, uppervalue|
+    Indicator.where(sigmahat_medium: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_probability_long, lambda { |lowervalue, uppervalue|
+    Indicator.where(probability_long: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_myuhat_long, lambda { |lowervalue, uppervalue|
+    Indicator.where(myuhat_long: lowervalue..uppervalue)
+  }
+
+  scope :filter_by_sigmahat_long, lambda { |lowervalue, uppervalue|
+    Indicator.where(sigmahat_long: lowervalue..uppervalue)
+  }
+
   def round
     indicators = {}
     indicators['symbol'] = symbol
