042478c3e377e9856575e86a514e73b22d4a0f07
MakiIchikawa <punkfolkfolk@gmail.com>
2022-02-11 12:32:40 +0900

    scopeを使用し、抽出するように変更

--- appserver/app/controllers/api/indicators_controller.rb
+++ appserver/app/controllers/api/indicators_controller.rb
@@ -1,29 +1,24 @@
 # frozen_string_literal: true
 
 class Api::IndicatorsController < ApplicationController
-  before_action :set_sql
+  before_action :set_symbols_array
   def index
-    @all_indicators = if @sql.empty?
-                        Indicator.order(:id).map(&:round)
-                      else
-                        Indicator.where(@sql).order(:id).map(&:round)
-                      end
+    @all_indicators = Indicator.filter_by_symbols(@symbols_array)
+                               .filter_by_price(params[:pricelowervalue], params[:priceuppervalue])
+                               .filter_by_probability_short(params[:probability_shortlowervalue], params[:probability_shortuppervalue])
+                               .filter_by_myuhat_short(params[:myuhat_shortlowervalue], params[:myuhat_shortuppervalue])
+                               .filter_by_sigmahat_short(params[:sigmahat_shortlowervalue], params[:sigmahat_shortuppervalue])
+                               .filter_by_probability_medium(params[:probability_mediumlowervalue], params[:probability_mediumuppervalue])
+                               .filter_by_myuhat_medium(params[:myuhat_mediumlowervalue], params[:myuhat_mediumuppervalue])
+                               .filter_by_sigmahat_medium(params[:sigmahat_mediumlowervalue], params[:sigmahat_mediumuppervalue])
+                               .filter_by_probability_long(params[:probability_longlowervalue], params[:probability_longuppervalue])
+                               .filter_by_myuhat_long(params[:myuhat_longlowervalue], params[:myuhat_longuppervalue])
+                               .filter_by_sigmahat_long(params[:sigmahat_longlowervalue], params[:sigmahat_longuppervalue])
   end
 
   private
 
-  def set_sql
-    queryparams = params.permit!.to_h.each_with_object([]) do |(key, value), params|
-      if key == 'symbol'
-        params << value.split(',')
-                       .map { |symbol| "symbol = #{symbol}" }
-                       .join(' or ')
-      elsif key.include?('uppervalue')
-        params << "#{key.gsub(/uppervalue/, '')} <= #{value}"
-      elsif key.include?('lowervalue')
-        params << "#{key.gsub(/lowervalue/, '')} >= #{value}"
-      end
-    end
-    @sql = queryparams.empty? ? '' : queryparams.join(' and ')
+  def set_symbols_array
+    @symbols_array = params[:symbol].nil? ? nil : params[:symbol].split(',')
   end
 end
