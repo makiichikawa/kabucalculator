12314742ab0935be341f5572976ac1751d667050
MakiIchikawa <punkfolkfolk@gmail.com>
2022-02-13 17:34:43 +0900

    バリデーションのテストを追加

--- appserver/test/system/indicators_test.rb
+++ appserver/test/system/indicators_test.rb
@@ -31,4 +31,60 @@ class IndicatorsTest < ApplicationSystemTestCase
       assert_text '1236'
     end
   end
+
+  test 'validate symbol' do
+    visit indicators_path
+    fill_in 'symbol', with: '123'
+    within('.v-form') do
+      assert_text '4桁の整数のみ有効。'
+    end
+    fill_in 'symbol', with: '1234, 1235'
+    within('.v-form') do
+      assert_text 'スペースでつなげると複数検索可(1234 1235)'
+    end
+    click_on '絞り込み'
+    within('.v-data-table') do
+      assert_selector('td', count: 44)
+      assert_text '1234'
+      assert_text '1235'
+      assert_text '1236'
+      assert_text '1237'
+    end
+  end
+
+  test 'validate price' do
+    visit indicators_path
+    fill_in 'price-lower-value', with: '-100'
+    fill_in 'price-upper-value', with: 'hoge'
+    within('.v-form') do
+      assert_text '正の数のみ有効'
+      assert_text '数値のみ有効'
+    end
+    fill_in 'myuhat-short-lower-value', with: '0'
+    click_on '絞り込み'
+    within('.v-data-table') do
+      assert_selector('td', count: 44)
+      assert_text '1234'
+      assert_text '1235'
+      assert_text '1236'
+      assert_text '1237'
+    end
+  end
+
+  test 'validate probability' do
+    visit indicators_path
+    fill_in 'probability-long-lower-value', with: '-100'
+    within('.v-form') do
+      assert_text '-50～50のみ有効'
+    end
+    fill_in 'myuhat-medium-lower-value', with: '-0.02'
+    click_on '絞り込み'
+    within('.v-data-table') do
+      assert_selector('td', count: 44)
+      assert_text '1234'
+      assert_text '1235'
+      assert_text '1236'
+      assert_text '1237'
+    end
+  end
 end
