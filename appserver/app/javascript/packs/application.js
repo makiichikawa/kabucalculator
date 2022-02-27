2573a80bc87c118b328b8363189b25f91e48797b
MakiIchikawa <punkfolkfolk@gmail.com>
2022-02-27 22:08:32 +0900

    ヘッダーの設置

--- appserver/app/javascript/packs/application.js
+++ appserver/app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import 'bootstrap'
-import '../stylesheets/application'
 import '../App.js'
+require.context("../images", true)
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
