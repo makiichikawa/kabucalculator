5f78a4c5f9f7e849812986d92549437ecd805708
MakiIchikawa <punkfolkfolk@gmail.com>
2022-04-03 12:46:40 +0900

    補足説明ページを修正

--- appserver/app/javascript/vue-additionals/additionals-page.vue
+++ appserver/app/javascript/vue-additionals/additionals-page.vue
@@ -3,12 +3,14 @@
     style="background: var(--v-base-base)"
   )
     v-container
-      .text-sm-body-1.py-12
+      .text-sm-h3
+        | 統計指標について
+      .text-sm-body-1.py-10
         | KabuCalculatorで計算されている指標はすべて、株価は対数正規分布に従うという前提で計算したものになります。
         br
         | シンプルな株式モデルとなりますが、株式同士で比較する一つの目安として指標を使用いただけたらと思います。
       v-row
-        v-col(cols='12' sm='12' md='10')
+        v-col(cols='12')
           v-card(height='100%')
             v-card-title
               | 上昇率、下降率
@@ -21,7 +23,7 @@
                 | -50%に近いほど価格水準が低いことを示し、50%に近いほど価格水準が高いことを示しています。
                 br
                 | 0%は株価の成長率を加味した上で過去の価格水準と今の価格水準が同じであることを示します。
-        v-col(cols='12' sm='12' md='10')
+        v-col(cols='12')
           v-card(height='100%')
             v-card-title
               | リターン
@@ -30,7 +32,7 @@
                 | 株価の対数収益率の平均になります。
               p
                 | 値が大きいほど、収益率つまりリターン大きい株式ということになります。
-        v-col(cols='12' sm='12' md='10')
+        v-col(cols='12')
           v-card(height='100%')
             v-card-title
               | リスク
@@ -39,7 +41,7 @@
                 | 株価の対数収益率の標準偏差になります。
               p
                 | 値が大きいほど、価格変動が大きい、つまりリスクの大きい株式ということになります。
-        v-col.text-center(cols='12' sm='12' md='10')
+        v-col.text-center(cols='12')
           v-btn(
             color="success"
             :href="`/indicators`"
@@ -49,4 +51,10 @@
 </template>
 <style lang='scss' scoped>
 @import '../stylesheets/_btn.scss';
+::v-deep .container {
+  max-width: 43rem;
+  .text-sm-h3 {
+    border-bottom: solid 1px var(--v-primary-base);
+  }
+}
 </style>
