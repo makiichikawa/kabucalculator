3c1505046437847241d82da96db9f0f048b7c057
makiichikawa <punkfolkfolk@gmail.com>
2023-07-02 13:25:18 +0900

    ビルドしてECRにプッシュするように設定してみる

---
+++ bin/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# アプリケーション名
+export APP_NAME=kabucalculator
+export AWS_ACCOUNT_ID=951034239227
+# ecrのリポジトリ
+export CONTAINER_REGISTRY=${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com
